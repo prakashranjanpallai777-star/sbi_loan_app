00effbb5a8e2bbddd0201b4ba4980c9713bf2fac
PrakashR <prakashranjanpallai777@gmail.com>
2026-07-02 11:16:11 +0530

    java file modified

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,5 +6,7 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int p = 10;
+        int j = 20;
     }
 }
